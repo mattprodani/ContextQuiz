083b6b530c6d67915981a5a0c19d0c043c70ab66
Mathias Prodani <mp5908@nyu.edu>
2021-12-12 21:10:06 -0500

    two modes

--- sketch.js
+++ sketch.js
@@ -3,10 +3,14 @@ let questionUrls = ["sicily.png", "kentucky.png"];
 let Q_COUNT;
 let imgs = [];
 let answers = [[]]
-let WIDTH;
-let HEIGHT;
+let score = 0;
+let responses = [];
+let resultTable = [];
+let quizMode = true;
+
 
 let questionData;
+let buttons = [];
 
 function preload(){
   questionData = loadJSON("src/questions.json", getImages);
@@ -22,61 +26,135 @@ function getImages(){
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  WIDTH = windowWidth;
-  HEIGHT = windowHeight;
+  makeButtons();
   
 
 }
 
 function draw() {
   background("#222");
+  if(quizMode){
+    showQuiz();
+  }else{
+    showResults();
+  }
+
+
+}
+
+
+function showResults(){
+
+  textSize(28);  
+  textAlign(CENTER);  
+  fill("#FFF")
+  text("Your Results", windowWidth / 2, windowHeight/10);
+  showResultRows();
+}
+
+function showResultRows(){
+  textSize(15);
+  for(let i = 0; i < resultTable.length; i++){
+    text("Question " + (i + 1) + ": " + resultTable[i][0], windowWidth / 2, i * windowHeight /3 + windowHeight / 5);
+    text("Correct answer: " + (i + 1) + ": " + resultTable[i][1], windowWidth / 2,i * windowHeight /3 +  windowHeight / 5  + windowHeight / 10);
+    text("Your answer: " + (i + 1) + ": " + resultTable[i][2], windowWidth / 2, i * windowHeight /3 + windowHeight / 5  + 2 * windowHeight / 10);
+  }
+}
+
+function showQuiz(){
   imageMode(CENTER);
   
+  showScore();
+
   showQuestionNumber();
 
 
   showButtons();
 
   showPhoto();
-
-
 }
 
 
+function showScore(){
+  textAlign(CENTER);
+  textSize(23);  
+  fill("#FFF");
+  text("Score: " + score, 50, 50);
+}
 
 function showQuestionNumber(){
   textSize(28);  
   textAlign(CENTER);  
   fill("#FFF")
-  text("Question " + (questionNo + 1), windowWidth / 2, windowHeight/5);
+  text("Question " + (questionNo + 1), windowWidth / 2, windowHeight/10);
+
+  textSize(22);
+  text("Pick the most appropriate caption", windowWidth / 2, windowHeight / 2 + windowHeight/20);
 
 }
 function showPhoto(){
-  image(imgs[questionNo], WIDTH/2, HEIGHT/2, 300, 200);
+  let height = windowHeight/10 * 3;
+  let start = windowHeight / 5;
+  image(imgs[questionNo], windowWidth/2, start + height / 2, height * (3/2), height);
 }
 
-function showButtons(){
-  console.log(questionData.labelCount);
-  // for(let i = 0; i < questionData.labelCount; i++){
+function showButtons(hide = false){
+  for(let i = 0; i < buttons.length; i++){
+    if(hide){
+      buttons[i].hide();
+    }else{
+    buttons[i].show();}
+  }
+}
 
-  // }
+function refresh(){
+  //BUTTONS
+  console.log("CALLED");
+  let label = "";
+  for(let i = 0; i < buttons.length; i++){
+    buttons[i].html(questionData.photos[questionNo].LABELS[i]);
+    buttons[i].center('horizontal');
+  }
+}
+
+function makeButtons(){
   let name = "";
   for(let i = 0; i < questionData.labelCount; i++){
     name = questionData.photos[questionNo].LABELS[i];
-    createButton(name, "label" + i)
-      .position(0, i * 50)
-      .mousePressed(changeImg(i))
-    console.log(i);
+    buttons[i] = createButton(name, "label" + i)
+      .position(windowWidth/2, windowHeight / 20 * (14 + i))
+      .mouseClicked(function(){ changeImg(i); }).center('horizontal');
   }
 }
 
 
+
 function changeImg(id){
-  // questionNo++;
-  console.log(id);
-  // clear();
+  let correct = "INCORRECT";
+  let correctId = questionData.photos[questionNo].CORRECT;
+  if(id == correctId){
+    score++;
+    correct = "CORRECT";
+  }
+  resultTable[questionNo] = [correct, questionData.photos[questionNo].LABELS[correctId], questionData.photos[questionNo].LABELS[id]];
+  responses[questionNo] = id;
+
+  if(questionNo == Q_COUNT - 1){
+    quizMode = false;
+    showButtons(true);
+    console.log(resultTable);
+  }else{
+    questionNo++;
+    refresh();}
+
+  clear();
+  
 }
+
 
 function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
+  for(let i = 0; i < buttons.length; i++){
+    buttons[i].position(windowWidth/2, windowHeight / 20 * (14 + i)).center('horizontal');
+  }
 }
